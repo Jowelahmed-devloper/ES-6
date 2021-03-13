b0ca0d1456b2e1e2cd674be4000d2d31e2ec6d7e
unknown <hello.jewelahmed@gmail.com>
2021-03-13 23:23:43 +0600

    three doot spread operator

---
+++ spread.js
@@ -0,0 +1,13 @@
+const ages =[12, 13, 14, 15]
+const ages1 = [17, 18, 19, 20]
+const ages2 = [23, 35, 38, 42]
+const allAges = [...ages, ...ages1, ...ages2]
+
+const business = 600;
+const minister = 400;
+const shocib = 300;
+
+const totalMoney = [600, 400, 300]
+const maximum = Math.max(...totalMoney);
+
+console.log(maximum);
