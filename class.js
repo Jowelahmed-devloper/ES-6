4fe9eb690aaf2b074c8d0a104dd0a3e826944a67
unknown <hello.jewelahmed@gmail.com>
2021-03-13 23:44:10 +0600

    class practice

---
+++ class.js
@@ -0,0 +1,10 @@
+class student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.university = "City University";
+    }
+}
+const student1 = new student(181385108, "Jowel");
+const student2 = new student(181385107, "Mamun");
+console.log(student1, student2);
