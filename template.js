877ac9ca67221c2104cf901db0cd9a044c8e3ed9
unknown <hello.jewelahmed@gmail.com>
2021-03-13 21:43:17 +0600

    practice Es-6

---
+++ template.js
@@ -0,0 +1,14 @@
+const firstName = 'Justin'
+const lastName = 'TimberLake'
+const fullName = firstName +" " + lastName;
+// Using Caret
+const fullName2 =`${firstName} ${lastName} is a good boy`
+
+
+
+// Multiline using caret
+const multiLine = `Hi i am Jowel Ahmed
+I am Javascript Developer
+I love to learn Javascript`
+
+console.log(multiLine, multiLine1);
